526c1de1a7df07025a1ed016e5f60cb13ca588d5
Abhkk9 <Abhshk.k98@gmail.com>
2025-06-17 22:39:11 +0530

    feat: Add Instamart component and routing
    - Added a new Instamart component with a welcome message and shopping button.
    - Implemented lazy loading for the Instamart component in App.js.
    - Updated Header component to include a link to the Instamart page.
    - Enhanced Body component's search button with Tailwind CSS styles.
    - Created a new RestaurantMenuCard component with a placeholder message.
    - Added a utility function to generate restaurant menu URLs in constants.js.
    - Updated package.json to include PostCSS and Tailwind CSS as dependencies.

---
+++ src/components/Instamart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Instamart() {
+  return (
+    <div>
+      <h1>Instamart</h1>
+      <p>Welcome to Instamart, your one-stop shop for all your grocery needs!</p>
+      <button className="shop-button">Start Shopping</button>
+      <p>Explore a wide range of products and enjoy great deals!</p>
+      <p>Happy shopping!</p>
+      <p>lazy loading this component</p>
+    </div>
+  )
+}
+
+export default Instamart
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const SWIGGY_LOGO =
   "https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png";
+  
+  export const getRestaurantMenuUrl = (resId) =>
+  `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.570317&lng=77.3218196&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`;
