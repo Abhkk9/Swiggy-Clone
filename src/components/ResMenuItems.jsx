2223adffea618e934ef56a936a4030cf1da177e5
Abhkk9 <Abhshk.k98@gmail.com>
2025-06-19 18:20:00 +0530

    feat: Refactored restaurant menu page and nested components , implemented collapsable Accordion, fixed logic for data filter from deeply nested restaurant API

---
+++ src/components/ResMenuItems.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {MENU_ITEM_IMAGE_API} from "../utils/constants";
+function ResMenuItems(props) {
+  const data = props.data;
+  return (
+    <div className="menu-item-card bg-white shadow-md rounded-lg p-3 m-2 flex items-center max-w-md">
+      <div className="flex-1 flex flex-col justify-center space-y-2">
+        <div className="flex items-center space-x-2">
+          <span
+            className={`inline-block w-3 h-3 rounded-full border-2 ${
+              data.isVeg
+                ? "border-green-600 bg-green-600"
+                : "border-red-600 bg-red-600"
+            }`}
+            title={data.isVeg ? "Vegetarian" : "Non-Vegetarian"}
+          ></span>
+          <h2 className="menu-data-name font-semibold text-base">
+            {data.name}
+          </h2>
+        </div>
+        <p className="menu-data-price text-gray-700 text-sm">
+          Price: ₹{(data.finalPrice || data.defaultPrice) / 100}
+        </p>
+      </div>
+      <div className="relative flex flex-col items-center">
+        <img
+          src={MENU_ITEM_IMAGE_API + data.imageId}
+          alt={data.name}
+          className="w-24 h-24 object-cover rounded-lg"
+        />
+        <button
+          className="absolute left-1/2 -translate-x-1/2 bottom-2 bg-white text-green-600 text-center px-3 py-1 rounded-md shadow-md hover:bg-gray-400 transition text-sm font-medium"
+          style={{ minWidth: "60px" }}
+        >
+          ADD
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ResMenuItems
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,7 @@ export const SWIGGY_LOGO =
 
 export const MENU_ITEM_IMAGE_API = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/";  
 
+export const RES_MENU_CATEGORY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+
   export const Restaurant_MENU_API = (resId) =>
   `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.570317&lng=77.3218196&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`;
